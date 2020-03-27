9853c92979b8a40036341b39a0123398372c28f1
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2020-03-27 11:35:23 +0900

    update structure

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@
  * @see {@link ../package.json}
  */
 
-import Group from './components/group/Group.svelte'
+import Group from './group/Group.svelte'
 
-import Bar from './components/shape/Bar.svelte'
-import Circle from './components/shape/Circle.svelte'
-import LinePath from './components/shape/LinePath.svelte'
+import Bar from './shape/Bar.svelte'
+import Circle from './shape/Circle.svelte'
+import LinePath from './shape/LinePath.svelte'
 
-import LinearGradient from './components/gradient/LinearGradient.svelte'
-import GradientPinkRed from './components/gradient/GradientPinkRed.svelte'
-import GradientTealBlue from './components/gradient/GradientTealBlue.svelte'
+import LinearGradient from './gradient/LinearGradient.svelte'
+import GradientPinkRed from './gradient/GradientPinkRed.svelte'
+import GradientTealBlue from './gradient/GradientTealBlue.svelte'
 
 const group = {
     Group,
